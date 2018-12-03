30627c2fa4dee78a31fdc051e0c4c1d959694064
Jerome Tabiri <jtabiri@gmail.com>
2018-12-03 12:53:49 +0000

    Updates

---
+++ precache-manifest.5240f6338ff856cd870922464172a05b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a50bb94574ec32d3449a9f3f4afca10f",
+    "url": "/cassava-ind/static/media/factory.a50bb945.png"
+  },
+  {
+    "revision": "50f0ff1fd4d80b59168a",
+    "url": "/cassava-ind/static/css/main.bcf6d0b4.chunk.css"
+  },
+  {
+    "revision": "59fe68e4d4fb6f6ebc88e7891f3a9de6",
+    "url": "/cassava-ind/static/media/bag_machine1.59fe68e4.png"
+  },
+  {
+    "revision": "a9e355360feb2cbc16b6",
+    "url": "/cassava-ind/static/js/1.a9e35536.chunk.js"
+  },
+  {
+    "revision": "02e0577faa27c71255ea",
+    "url": "/cassava-ind/static/js/runtime~main.02e0577f.js"
+  },
+  {
+    "revision": "4e528c6af47d1ef614f5d3f4774dd24c",
+    "url": "/cassava-ind/static/media/items.4e528c6a.jpeg"
+  },
+  {
+    "revision": "5b9821e8ea120d332975c302fd8b9051",
+    "url": "/cassava-ind/static/media/plates.5b9821e8.jpeg"
+  },
+  {
+    "revision": "50f0ff1fd4d80b59168a",
+    "url": "/cassava-ind/static/js/main.50f0ff1f.chunk.js"
+  },
+  {
+    "revision": "8fbdcdd0b087ecc5858fc709cb2c957e",
+    "url": "/cassava-ind/static/media/entrance.8fbdcdd0.png"
+  },
+  {
+    "revision": "19faa91fa2452e76ccfbb5517b39d3d0",
+    "url": "/cassava-ind/static/media/mail-icon-logo.19faa91f.png"
+  },
+  {
+    "revision": "bb3cf2990833a034e1f3f4ada41f4486",
+    "url": "/cassava-ind/static/media/phone-icon.bb3cf299.png"
+  },
+  {
+    "revision": "6869d3dfe2d180faa0d0e0ea9cbcf267",
+    "url": "/cassava-ind/static/media/greenportugal.6869d3df.jpg"
+  },
+  {
+    "revision": "b605a658dfcea1f6643ae636e24cf4a1",
+    "url": "/cassava-ind/static/media/cassavafarm.b605a658.jpg"
+  },
+  {
+    "revision": "a9e355360feb2cbc16b6",
+    "url": "/cassava-ind/static/css/1.228842b9.chunk.css"
+  },
+  {
+    "revision": "6877c6998d546286eb539410c809080d",
+    "url": "/cassava-ind/index.html"
+  }
+];
